ed5b47284661868a6bf5aea722bfa84a494d80f9
imaliabdullah <aliabdullah4545@gmail.com>
2024-06-05 16:21:22 +0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -147,31 +147,21 @@ window.addEventListener('scroll', scrollUp)
 
 
 /*==================== DARK LIGHT THEME ====================*/ 
-const themeButton = document.getElementById('theme-button')
-const darkTheme = 'dark-theme'
-const iconTheme = 'uil-sun'
+const themeButton = document.getElementById('theme-button');
 
-// Previously selected topic (if user selected)
-const selectedTheme = localStorage.getItem('selected-theme')
-const selectedIcon = localStorage.getItem('selected-icon')
+// Function to toggle between dark and light themes
+const toggleTheme = () => {
+    document.body.classList.toggle('dark-theme');
+    themeButton.classList.toggle('uil-sun');
+};
 
-// We obtain the current theme that the interface has by validating the dark-theme class
-const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
-const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun'
-
-// We validate if the user previously chose a topic
-if (selectedTheme) {
-  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
-  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
-  themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme)
-}
-
-// Activate / deactivate the theme manually with the button
+// Event listener to toggle theme when button is clicked
 themeButton.addEventListener('click', () => {
-    // Add or remove the dark / icon theme
-    document.body.classList.toggle(darkTheme)
-    themeButton.classList.toggle(iconTheme)
-    // We save the theme and the current icon that the user chose
-    localStorage.setItem('selected-theme', getCurrentTheme())
-    localStorage.setItem('selected-icon', getCurrentIcon())
-})
+    toggleTheme();
+    // Save the theme preference to local storage
+    localStorage.setItem('theme', document.body.classList.contains('dark-theme') ? 'dark' : 'light');
+});
+
+// Set the theme based on user preference from local storage
+document.body.classList.add(localStorage.getItem('theme') === 'dark' ? 'dark-theme' : '');
+themeButton.classList.add(localStorage.getItem('theme') === 'dark' ? 'uil-sun' : 'uil-moon');
